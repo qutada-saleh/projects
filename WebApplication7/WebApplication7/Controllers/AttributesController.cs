fc3aca54200bfacc34ff9be1b1e706e053fce6cf
qutada saleh <qutada213@gmail.com>
2022-11-29 17:40:23 +0200

    initial

---
+++ WebApplication7/WebApplication7/Controllers/AttributesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication7.Controllers
+{
+    public class AttributesController : ApiController
+    {
+
+        public IEnumerable<Attributes> Get()
+        {
+            using (ExpertSystemEntities entities = new ExpertSystemEntities())
+            {
+                return entities.Attributes.ToList();
+            }
+        }
+
+        public Attributes Get(int id)
+        {
+            using (ExpertSystemEntities entities = new ExpertSystemEntities())
+            {
+                return entities.Attributes.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
